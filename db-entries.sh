c55d4d68687f8404fb52895e7ce502077911efae
dukaludic <dukaludic@gmail.com>
2023-10-12 20:57:32 +0200

    count

--- db-entries.sh
+++ db-entries.sh
@@ -7,23 +7,43 @@ DB_HOST="127.0.0.1"
 DB_PORT="3306"
 DB_NAME="db"
 
-tables=$(mysql -u"$DB_USER" -p''"$DB_PASSWORD"'' -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" -e "SHOW TABLES;" | tail -n +2)
+# Function to execute a MySQL query
+execute_mysql_query() {
+    local query="$1"
+    mysql --user="$DB_USER" --password="$DB_PASSWORD" -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" --skip-column-names --batch -e "$query"
+}
+
+# Function to print a table's latest entry
+print_latest_entry() {
+    local table="$1"
+    echo -e "\e[32m${table^^}\e[0m"
+    execute_mysql_query "SELECT * FROM $table ORDER BY id DESC LIMIT 1;"
+}
+
+# Function to get the count of records in a table
+get_record_count() {
+    local table="$1"
+    echo $(execute_mysql_query "SELECT COUNT(*) FROM $table")
+}
+
+tables=$(mysql --user="$DB_USER" --password=''"$DB_PASSWORD"'' -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" -e "SHOW TABLES;" | tail -n +2)
 
 if [[ $1 = "-ss" ]]; then
     exec 4>&1
-    exec > ss-output.txt
+    exec > /tmp/ss-output.txt
 
     for table in $tables; do
-        latest_id=$(mysql -u"$DB_USER" -p"$DB_PASSWORD" -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" -e "SELECT id FROM $table ORDER BY id DESC LIMIT 1;" | tail -n +2)
-        echo "$table:$latest_id"
+        count=$(get_record_count "$table")
+        echo "$table:$count"
     done
 
     exec 1>&4
     exec 4>&-
     echo "Snapshot taken"
+    exit 0
 elif [[ $1 = "-e" ]]; then
 
-    if [ ! -e "ss-output.txt" ]; then
+    if [ ! -e "/tmp/ss-output.txt" ]; then
         echo "Snapshot file does not exist."
         exit 1
     fi
@@ -32,37 +52,33 @@ elif [[ $1 = "-e" ]]; then
 
     while IFS=":" read -r table value; do
         table_values["$table"]=$value
-    done < "ss-output.txt"
+    done < "/tmp/ss-output.txt"
 
-    echo "New entries:"
+    echo "Differences:"
 
     for table in $tables; do
     if [[ -v table_values["$table"] ]]; then
-        latest_id="${table_values["$table"]}"
+        prev_count="${table_values["$table"]}"
 
-        mysql_output=$(mysql -u"$DB_USER" -p"$DB_PASSWORD" -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" -e "
-            SELECT * FROM $table
-            WHERE id > ${latest_id:-0};"
-        )
+        new_count=$(get_record_count "$table")
 
-        if [ -n "$mysql_output" ]; then
-            echo -e "\e[32m${table^^}\e[0m"
-            echo "$mysql_output"
+        if [ "$prev_count" != "$new_count" ]; then
+            diff=$((new_count - prev_count))
+            echo -e "\e[32m${table^^}\e[0m $diff"
         fi
     else
         echo "Value for $table not found in table_values."
     fi
     done
+    exit 0
 elif [[ $1 = "-l" ]]; then
     echo "Latest entries:"
 
     for table in $tables; do
     echo -e "\e[32m${table^^}\e[0m"
-    mysql -u"$DB_USER" -p"$DB_PASSWORD" -h"$DB_HOST" -P"$DB_PORT" -D "$DB_NAME" -e "
-        SELECT * FROM $table
-        ORDER BY id DESC
-        LIMIT 1;"
+    print_latest_entry "$table"
     done
+    exit 0
 else
     echo "Usage: flag -ss for snapshot of database, -e for entries after snapshot, -l for latest"
     exit 0;
